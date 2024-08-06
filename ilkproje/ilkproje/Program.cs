fca09484e1b8de16daf470b30890cfc00f142ab0
terhanseda77@gmail.com <terhanseda77@gmail.com>
2024-08-06 22:55:08 +0300

    int öğreniyorum

--- ilkproje/ilkproje/Program.cs
+++ ilkproje/ilkproje/Program.cs
@@ -194,3 +194,10 @@ bool isCSharpFun = true;
 bool isFishTasty = false;
 Console.WriteLine(isCSharpFun);   // Outputs True
 Console.WriteLine(isFishTasty);   // Outputs False
+
+
+int x = 10;
+int y = 9;
+Console.WriteLine(x > y); // returns True, because 10 is higher than 9
+
+Console.WriteLine(10 > 9); // returns True, because 10 is higher than 9
