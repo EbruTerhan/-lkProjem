d0e13276a52936274fdd71b9f9aa88dc69b94f12
terhanseda77@gmail.com <terhanseda77@gmail.com>
2024-08-06 22:39:01 +0300

    projenin temelleri atıldı

---
+++ ilkproje/Program.cs
@@ -0,0 +1,190 @@
+﻿
+/*
+Console.WriteLine(" Hello World !");
+Console.WriteLine(" I am learning C#");
+Console.WriteLine("İt is awesome! ");
+*/
+
+/*Console.WriteLine(3 + 3);*/
+
+/*Console.WriteLine(" Hello World!");
+Console.WriteLine("I will print on the same line.");*/
+
+/*String name = "john" ;
+Console.WriteLine(name);*/
+
+/*int myNum = 15;
+Console.WriteLine(myNum);*/
+
+/*int myNum;
+myNum = 15;
+Console.WriteLine(myNum);*/
+
+/*int myNume = 15;
+myNume = 20;
+Console.WriteLine(myNume);*/
+
+/*int myNum = 5;
+double myDoubleNum = 5.99D;
+char myLetter = 'D';
+bool myBool = true;
+string myText = "hello";*/
+
+/*Const int myNum =15;
+myNum = 20;*/
+
+/*string name = "john";
+Console.WriteLine("hello" + name);*/
+
+/*string firstName = "John";
+string lastName = "Doe";
+string fullName = firstName + lastName;
+Console.WriteLine(fullName);*/
+
+/*int x = 5;
+int y = 6;
+Console.WriteLine(x + y);*/
+
+/*int x = 5, y = 6, z = 50;
+Console.WriteLine(x + y + z);*/
+
+/*int x, y, z;
+x = y = z = 50;
+Console.WriteLine(x + y + z);*/
+
+/*int minutesPerHour = 60;
+int m = 60;*/
+
+/*int myNume = 100000;
+Console.WriteLine(myNume);*/
+
+/*long myNume = 10000000000000L;
+Console.WriteLine(myNume);*/
+
+/*float myNume = 5.75F;
+Console.WriteLine(myNume);*/
+
+//float f1 = 35e3F;
+//double d1 = 12e4D;
+//Console.WriteLine(f1);
+//Console.WriteLine(d1);
+
+/*bool isCSarpFun = true;
+bool isfishtasty = false;
+Console.WriteLine(isCSarpFun);
+Console.WriteLine(isfishtasty);*/
+/*
+char myGrade = 'B';
+Console.WriteLine(myGrade);*/
+/*
+string getting = "hello World";
+Console.WriteLine(gretting);*/
+/*
+double myDouble = 9.78;
+int myİnt = (int)myDouble;
+Console.WriteLine(myDouble);
+Console.WriteLine(myİnt);*/
+
+/*int myİnt = 10;
+double myDouble = 5.75;
+bool myBool = true;
+Console.WriteLine(Convert.ToString (myİnt));
+Console.WriteLine(Convert.ToDouble(myİnt));
+Console.WriteLine(Convert.ToInt32(myDouble));
+Console.WriteLine(Convert.ToString(myBool));*/
+
+//Console.WriteLine("Enter Username:");
+//String userName = Console.ReadLine();
+//Console.WriteLine("userName is:" + userName);
+
+
+//Console.WriteLine("Yaşınızı Girin:");
+//string yas = Console.ReadLine();
+//Console.WriteLine("Sizin yaşınız : " + yas);
+
+//Console.WriteLine("Seda Yaş:");
+//int sedaYas = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine("Zeki Yaş:");
+//int zekiYas = Convert.ToInt32(Console.ReadLine());
+
+//int toplam = sedaYas + zekiYas;
+//Console.WriteLine("zeki ve sedasının yaş toplam = "+ toplam);
+
+/*
+int x = 100 + 50;*/
+/*
+int Sum1 = 100 + 50;
+int Sum2 = Sum1 + 250;
+int Sum3 = Sum2 + Sum2;*/
+/*
+int x = 10;*/
+/*
+int x = 10;
+x += 5;*/
+/*
+int x = 5;
+int y = 3;
+Console.WriteLine(x > y);*/
+/*
+Math.Max(5, 10);*/
+/*
+Math.Min(5, 10);*/
+
+/*Math.Sqrt(64);*/
+
+/*Math.Abs(-4.7);*/
+
+/*string gretting = " kocammmmm";
+Console.WriteLine(gretting);*/
+
+
+/*
+Console.WriteLine(Math.Abs(-4.7));
+*/
+
+/*string gretting2 = ("Nice to meet you!");
+Console.WriteLine(gretting2);*/
+/*
+string txt = "ABCDEFGHIJKLMNOPQRSTUVWYZ";
+Console.WriteLine("The length of the txt string is :" + txt.Length);*/
+
+/*string txt = "Hello World";
+Console.WriteLine( txt.ToUpper());
+Console.WriteLine(txt.ToLower());*/
+
+/*string FirstName = "john";
+string lastName = " doe";
+string Name = FirstName + lastName;
+Console.WriteLine (Name);*/
+
+//string FirstName = "john";
+//string LastName = "doe";
+//string Name = string.Concat(FirstName, LastName);
+//Console.WriteLine(Name);
+
+//int x = 10;
+//int y = 20;
+//int z = x + y;
+
+//string FirtsName = "John";
+//string LastName = " Doe";
+//string Name =  " $ My full name is :{ FirtsName} { LastName}";
+//Console.WriteLine(Name);
+
+
+//string myString = "Hello";
+//Console.WriteLine(myString[0]);
+
+//string myString = "Hello";
+//Console.WriteLine(myString[1]);
+
+//string myString = "Hello";
+//Console.WriteLine(myString.IndexOf("e"));
+
+
+//string name = "John Doe";
+//int charPos = name.IndexOf("D");
+//string lastName = name.Substring(charPos);
+//Console.WriteLine(lastName);
+
